29b91194e06c2ee85c96d55e8676a40834a9217c
hugo-sullivan <81408574+hugo-sullivan@users.noreply.github.com>
2021-03-27 02:21:49 +1100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,203 @@
+let canvas = document.getElementById("gameScreen");
+let ctx = canvas.getContext("2d");
+
+const pi = 3.14159265358979;
+const width = canvas.width;
+const height = canvas.height;
+const boidNum = 700;
+const predNum = 20;
+const boidLimit = 5;
+const predLimit = 5;
+const flockCentering = 0.05;
+const velMatching = 0.05;
+const predAccel = 0.07;
+const visualDist = 20;
+
+var boids = [];
+var preds = [];
+
+function initBoid() {
+  for (var i = 0; i < boidNum; i++) {
+    var startVel = Math.random() * 4 + 1;
+    var startAngel = Math.random() * (2 * pi);
+    boids[i] = {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      dx: startVel * Math.cos(startAngel),
+      dy: startVel * Math.sin(startAngel)
+    };
+  }
+}
+
+function initPred() {
+  for (var i = 0; i < predNum; i++) {
+    var startVel = Math.random() * 4 + 1;
+    var startAngel = Math.random() * (2 * pi);
+    preds[i] = {
+      x: Math.random() * width,
+      y: Math.random() * height,
+      dx: startVel * Math.cos(startAngel),
+      dy: startVel * Math.sin(startAngel)
+    };
+  }
+}
+
+function distance(boid1, boid2) {
+  return Math.sqrt(
+    (boid1.x - boid2.x) * (boid1.x - boid2.x) +
+      (boid1.y - boid2.y) * (boid1.y - boid2.y)
+  );
+}
+
+// Move away from other boids that are too close to avoid colliding
+function avoidOthers(boid) {
+  const minDistance = 10; // The distance to stay away from other boids
+  const avoidFactor = 0.05; // Adjust velocity by this %
+  let moveX = 0;
+  let moveY = 0;
+  for (let i = 0; i < boidNum; i++) {
+    if (boids[i] !== boid && distance(boid, boids[i]) < minDistance) {
+      moveX += boid.x - boids[i].x;
+      moveY += boid.y - boids[i].y;
+    }
+  }
+
+  boid.dx += moveX * avoidFactor;
+  boid.dy += moveY * avoidFactor;
+}
+
+//Aim for center of boids in visual range and shift velocity
+//closer to the average if boids in visual range
+function visualAim(boid) {
+  let averageX = 0;
+  let averageY = 0;
+  let averageDX = 0;
+  let averageDY = 0;
+  let numBoid = 0;
+
+  for (let i = 0; i < boidNum; i++) {
+    if (boids[i] !== boid && distance(boid, boids[i]) < visualDist) {
+      averageY += boids[i].y;
+      averageX += boids[i].x;
+      averageDY += boids[i].dy;
+      averageDX += boids[i].dx;
+      numBoid++;
+    }
+  }
+
+  if (numBoid) {
+    boid.dy += (averageY / numBoid - boid.y) * flockCentering;
+    boid.dx += (averageX / numBoid - boid.x) * flockCentering;
+    boid.dy += (averageDY / numBoid - boid.dy) * velMatching;
+    boid.dx += (averageDX / numBoid - boid.dx) * velMatching;
+  }
+}
+
+function avoidPred(boid) {
+  const avoidFactor = 0.05; // Adjust velocity by this %
+  let moveX = 0;
+  let moveY = 0;
+  for (let i = 0; i < predNum; i++) {
+    if (preds[i] !== boid && distance(boid, preds[i]) < visualDist) {
+      moveX += boid.x - preds[i].x;
+      moveY += boid.y - preds[i].y;
+    }
+  }
+
+  boid.dx += moveX * avoidFactor;
+  boid.dy += moveY * avoidFactor;
+}
+
+function limitSpeed(boid, speedLimit) {
+  var speed = Math.sqrt(boid.dx * boid.dx + boid.dy * boid.dy);
+  if (speed > speedLimit) {
+    boid.dx = speedLimit * (boid.dx / speed);
+    boid.dy = speedLimit * (boid.dy / speed);
+  }
+}
+
+function drawTriangle(ctx, x, y, angle, hypot, fillStyle) {
+  ctx.beginPath();
+  ctx.moveTo(x, y);
+  ctx.lineTo(
+    x + hypot * Math.sin(angle + 0.261799387799),
+    y + hypot * Math.cos(angle + 0.261799387799)
+  );
+  ctx.lineTo(
+    x + hypot * Math.sin(angle - 0.261799387799),
+    y + hypot * Math.cos(angle - 0.261799387799)
+  );
+  ctx.closePath();
+  ctx.fillStyle = fillStyle;
+  ctx.fill();
+}
+
+function boidTick(ctx, boid) {
+  avoidOthers(boid);
+  visualAim(boid);
+  avoidPred(boid);
+  limitSpeed(boid, boidLimit);
+  const angle = Math.atan2(boid.dx, boid.dy) + pi;
+  boid.x += boid.dx;
+  boid.y += boid.dy;
+  boid.x = boid.x % width;
+  boid.y = boid.y % height;
+  if (boid.x < 0) {
+    boid.x = boid.x + width;
+  }
+  if (boid.y < 0) {
+    boid.y = boid.y + height;
+  }
+  drawTriangle(ctx, boid.x, boid.y, angle, 8, "blue");
+}
+
+function predAim(ctx, pred) {
+  let closestBoidDist = visualDist + 1;
+  let closestBoid = -1;
+  for (let i = 0; i < boidNum; i++) {
+    let dist = distance(pred, boids[i]);
+    if (dist < visualDist + 20 && dist < closestBoidDist) {
+      closestBoid = i;
+      closestBoidDist = dist;
+    }
+  }
+  if (closestBoid !== -1) {
+    pred.dy += (boids[closestBoid].y - pred.y) * predAccel;
+    pred.dx += (boids[closestBoid].x - pred.x) * predAccel;
+  }
+}
+
+function predTick(ctx, pred) {
+  predAim(ctx, pred);
+  limitSpeed(pred, predLimit);
+  const angle = Math.atan2(pred.dx, pred.dy) + pi;
+  pred.x += pred.dx;
+  pred.y += pred.dy;
+  pred.x = pred.x % width;
+  pred.y = pred.y % height;
+  if (pred.x < 0) {
+    pred.x = pred.x + width;
+  }
+  if (pred.y < 0) {
+    pred.y = pred.y + height;
+  }
+  drawTriangle(ctx, pred.x, pred.y, angle, 14, "red");
+}
+
+function animationLoop() {
+  ctx.clearRect(0, 0, width, height);
+  for (var i = 0; i < boidNum; i++) {
+    boidTick(ctx, boids[i]);
+  }
+  for (var j = 0; j < predNum; j++) {
+    predTick(ctx, preds[j]);
+  }
+
+  // Schedule the next frame
+  requestAnimationFrame(animationLoop);
+}
+
+initBoid();
+initPred();
+// Schedule the main animation loop
+requestAnimationFrame(animationLoop);
